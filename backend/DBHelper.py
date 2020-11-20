e3103ec2e0e149992d55f41125ca3b230390a405
rhitayu2 <rhitayu2>
2020-11-20 13:52:56 +0530

    error message

--- backend/DBHelper.py
+++ backend/DBHelper.py
@@ -1,8 +1,14 @@
 import pymysql
+import sys
 
-connection = pymysql.connect(host='localhost',
+try:
+	connection = pymysql.connect(host='localhost',
                             user='rhitayu_smarthome',
                             password='smarthome',
                             db='SmartHomeFlask',
                             charset='utf8mb4',
                             cursorclass=pymysql.cursors.DictCursor)
+except:
+	print("[-] MySQL Server is not reachable...")
+	print("[-] Exiting...")
+	sys.exit(0)
